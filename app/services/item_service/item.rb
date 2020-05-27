092627a265d051725ba04fcec8a69e05ddc56e5e
Ahrry <gopalahrry@gmail.com>
2020-05-27 15:46:17 +0200

    refacto, ok

--- app/services/item_service/item.rb
+++ app/services/item_service/item.rb
@@ -6,56 +6,9 @@ class ItemService::Item
   attr_reader :item
 
   def next_day!
+    module_name = 'ItemService'
+    class_name = "item_#{@item.item_type}".camelcase
 
-    if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-      if item.quality > 0
-        if item.name != "Sulfuras, Hand of Ragnaros"
-          item.quality = item.quality - 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality = item.quality + 1
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-        end
-      end
-    end
-
-    if item.name != "Sulfuras, Hand of Ragnaros"
-      item.sell_in = item.sell_in - 1
-    end
-
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != "Backstage passes to a TAFKAL80ETC concert"
-          if item.quality > 0
-            if item.name != "Sulfuras, Hand of Ragnaros"
-              item.quality = item.quality - 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-        end
-      end
-    end
-
-    item.save
+    "#{module_name}:#{class_name}".constantize.new(@item).next_day!
   end
-
 end
-
-
